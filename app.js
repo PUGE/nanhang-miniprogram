5f769c048b7a4594f4375280bb6e8ff82aec3395
puge <mail@puge.org>
2020-09-22 23:43:17 +0800

    添加统计代码

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 //app.js
+var mta= require('./mta_analysis')
 App({
   globalData: {
     openid: null
   },
+  onLaunch: function (options) {
+    console.log(mta)
+    mta.App.init({
+      "appID":"500730176",
+      "autoReport": true,
+  	  "statParam": true,
+      "lauchOpts": options,
+      "ignoreParams": [],
+    })
+  }
 })
